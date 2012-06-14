14a4fbcb67a6ccf015f3e45b39398514e526b1b0
u39720 <u39720@53f0f008-4f8b-4db3-9e77-67d1220605a3.(none)>
2012-06-14 00:58:23 +0000

    Created by pythonpackages.com

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+VERSION = '0.0.1'
+
+setup(
+    author='aclark4life',
+    include_package_data=True,
+    name='event_days_indexer',
+    packages=find_packages(),
+    version=VERSION,
+)
