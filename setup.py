f80658cd7853bc5cb7445e45db32751382ddc611
Alex Clark <aclark@aclark.net>
2012-06-14 19:21:38 -0400

    Add gs

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import find_packages
 from setuptools import setup
 
-
 VERSION = '0.0.1'
+
 
 setup(
     author='aclark4life',
+    entry_points={
+        'z3c.autoinclude.plugin': 'target = plone',
+    },
     include_package_data=True,
     name='event_days_indexer',
     packages=find_packages(),
