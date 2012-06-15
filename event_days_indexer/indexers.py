98c79aa232de6f3f28801a141e9dfa9080641492
Alex Clark <aclark@aclark.net>
2012-06-14 20:34:35 -0400

    Add adapter

---
+++ event_days_indexer/indexers.py
@@ -0,0 +1,15 @@
+
+# register this as documented at
+#   http://collective-docs.readthedocs.org/en/latest/searching_and_indexing/indexing.html#custom-index-methods
+
+from plone.indexer.decorator import indexer
+from Products.ATContentTypes.interface import IATEvent
+from Products.ATContentTypes.utils import DT2dt
+
+
+@indexer(IATEvent)
+def event_days(context, **kw):
+    start = DT2dt(context.getStartDate())
+    end = DT2dt(context.getEndDate())
+    delta = end - start
+    return delta.days
